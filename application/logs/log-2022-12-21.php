4ff4014d7e061374e06ae65123490ffcf26f58f9
prima23 <prima.aulia333@gmail.com>
2022-12-29 09:14:25 +0700

    perbaiki javascript

---
+++ application/logs/log-2022-12-21.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-12-21 09:38:45 --> 404 Page Not Found: /index
+ERROR - 2022-12-21 13:40:42 --> 404 Page Not Found: /index
---
+++ application/logs/log-2022-12-26.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-12-26 09:33:14 --> 404 Page Not Found: /index
+ERROR - 2022-12-26 09:45:40 --> 404 Page Not Found: /index
